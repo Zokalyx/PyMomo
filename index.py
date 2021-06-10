8a768fd2b7602ff3aea08c1cad90d7f07738bb55
Zokalyx <zokalyx@gmail.com>
2021-06-10 16:08:48 -0300

    First steps

--- index.py
+++ index.py
@@ -2,14 +2,23 @@ from os import getenv
 from dotenv import load_dotenv
 import discord
 
+# Load .env variables
 load_dotenv("./.env")
 
-class MyClient(discord.Client):
+# Import replit if on cloud
+if not int(getenv("LOCAL")):
+    from replit import db
+
+# Momo class
+class Momo(discord.Client):
     async def on_ready(self):
         print('Logged in!'.format(self.user))
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
-client = MyClient()
+# Initialize momo
+momo = Momo()
+
+# Connect to Discord
 client.run(getenv("PYMOMO_TOKEN"))
